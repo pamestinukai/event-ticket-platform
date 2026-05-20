ce934e263f7c0273f4fac826f5333eed34ab3888
Titas Vaitkus <titasvaitkus@gmail.com>
2026-05-20 17:24:41 +0300

    Added event catalog

--- frontend/src/api/events.ts
+++ frontend/src/api/events.ts
@@ -1,6 +1,22 @@
 import { API_BASE_URL } from "../constants";
 import type {EventResponse} from "../types/EventResponse.ts";
 
+export async function getAvailableEvents(): Promise<EventResponse[]> {
+    const response = await fetch(`${API_BASE_URL}/api/events/public/available`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    });
+    if (!response.ok) {
+        await response.json().catch(() => null);
+        switch (response.status) {
+            case 401: throw new Error("Unauthorized");
+            case 403: throw new Error("Forbidden");
+            default: throw new Error("Failed to fetch events");
+        }
+    }
+    return await response.json();
+}
+
 export async function getEventById(id: string): Promise<EventResponse> {
     const response = await fetch(`${API_BASE_URL}/api/events/public/${id}`, {
         method: "GET",
